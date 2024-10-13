47f3120df1bb3a229781317737823b7a6415ca15
kjh9852 <kjh985264@gmail.com>
2024-10-14 00:37:55 +0900

    feat: 공용 Container 컴포넌트를 제작한다 #22

--- components/@common/Container.tsx
+++ components/@common/Container.tsx
@@ -6,8 +6,8 @@ interface ContainerProps {
 
 export function Container({ children }: ContainerProps) {
   return (
-    <section className="mx-auto w-full max-w-[1200px]">
-      <div className="px-0 xs:px-6 md:px-4 xl:px-0">{children}</div>
+    <section className="max-w-pc mx-auto w-full">
+      <div className="px-6 tab:px-4 pc:px-0">{children}</div>
     </section>
   );
 }
